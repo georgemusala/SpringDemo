a9d50e6ec787ac4cee822cc9d95a65bfcff55e12
george.musala <george.musala@gmail.com>
2016-11-24 15:45:14 +0200

    add Bean Definition Inheritance

--- src/org/koushik/javabrains/Triangle.java
+++ src/org/koushik/javabrains/Triangle.java
@@ -1,27 +1,22 @@
 package org.koushik.javabrains;
 
-import java.util.List;
-
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-
-public class Triangle implements ApplicationContextAware, BeanNameAware {
+public class Triangle {
     
-    private Point pointA;
-    private Point pointB;
-    private Point pointC;
-    private ApplicationContext context = null;
-    
-	public Point getPointA() {
-		return pointA;
-	}
-
-	public void setPointA(Point pointA) {
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	
+	public void setPointA(Point pointA)
+	{
 		this.pointA = pointA;
 	}
-
+	
+	public Point getPointA()
+	{
+		return pointA;
+	}
+	
+	
 	public Point getPointB() {
 		return pointB;
 	}
@@ -39,20 +34,8 @@ public class Triangle implements ApplicationContextAware, BeanNameAware {
 	}
 
 	public void draw(){
-		System.out.println("Point A = (" + pointA.getX() + "," + pointA.getY() + ")");
-		System.out.println("Point B = (" + pointB.getX() + "," + pointB.getY() + ")");
-		System.out.println("Point C = (" + pointC.getX() + "," + pointC.getY() + ")");
-     }
-
-	@Override
-	public void setApplicationContext(ApplicationContext context) throws BeansException {
-		this.context = context;
-		
-	}
-
-	@Override
-	public void setBeanName(String beanName) {
-		System.out.println("Bean name is: "+ beanName);
-		
-	}
+        System.out.println("Point A = (" + getPointA().getX() + "," + getPointA().getY() + ")");
+        System.out.println("Point B = (" + getPointB().getX() + "," + getPointB().getY() + ")");
+        System.out.println("Point C = (" + getPointC().getX() + "," + getPointC().getY() + ")");
+    }
 }
