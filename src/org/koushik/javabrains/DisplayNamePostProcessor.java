fae3c1c646e956bf77b7083b462013d323fc6422
george.musala <george.musala@gmail.com>
2016-11-25 10:24:02 +0200

    add BeanPostProcessor

---
+++ src/org/koushik/javabrains/DisplayNamePostProcessor.java
@@ -0,0 +1,21 @@
+package org.koushik.javabrains;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class DisplayNamePostProcessor implements BeanPostProcessor{
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("In After initialization method. Bean name is " + beanName);
+		return bean;
+	}
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("In Before initialization method. Bean name is " + beanName);
+		return bean;
+		
+	}
+
+}
